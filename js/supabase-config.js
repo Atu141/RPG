55ab976393922e98e455fe8c4f50452e9353928c
Atu141 <badu.arthur@hotmail.com>
2026-09-07 01:58:26 -0300

    Add BD

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -4,6 +4,6 @@
  * Nunca coloque aqui a chave service_role. Use somente a chave publishable/anon.
  */
 window.SUPABASE_CONFIG = {
-  url: 'COLE_AQUI_A_URL_DO_SEU_PROJETO',
-  anonKey: 'COLE_AQUI_A_CHAVE_PUBLICAVEL_DO_SUPABASE'
+  url: 'https://lgaglkppdizwohlwroal.supabase.co',
+  anonKey: 'sb_publishable_mnDdK6Pt2gbsKbqnAGaJRw_mJGh5jYO'
 };
